c083cd5015b581bb49fd4f616d0b1e7a43b77799
schragman <schragman@arcor.de>
2018-09-27 23:55:27 +0200

    [FIX]: Putting Service in the WAR

---
+++ micWEB/src/main/java/controller/BookRestController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.net.URI;
+
+import javax.ejb.EJB;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import beans.BookBean;
+import entities.Book;
+
+//http://localhost:8080/ReferenceII/rest/books
+@Path("/books")
+@ApplicationPath("/rest")
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+public class BookRestController extends Application{
+
+	@Context
+	private UriInfo uriInfo;
+
+	@EJB
+	private BookBean bookBean;
+
+	//http://localhost:8080/ReferenceII/rest/books/2
+	@GET
+	@Path("{id}/")
+	public Response getUriBookById(@PathParam("id") Long id) {
+		Book book = bookBean.findBookById(id);
+		//URI bookUri = uriInfo.getAbsolutePathBuilder().path(book.getId().toString()).build();
+
+		return Response.ok(book).build();
+		//return "say Michael";
+	}
+
+}
